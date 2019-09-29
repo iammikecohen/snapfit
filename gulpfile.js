4827f9e716430b0de167fe4a2c457c0209074470
Michael Cohen <michael@fatastronauts.com>
2019-09-29 18:21:37 -0500

    mostly quote changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,30 @@
 /* globals require */
-var gulp = require('gulp');
-var del = require('del');
-var jscadFiles = require('gulp-jscad-files');
-var merge2 = require('merge2');
-var debug = require('gulp-debug');
-var inject = require('gulp-inject');
-var plumber = require('gulp-plumber');
-var terser = require('gulp-terser');
+var gulp = require("gulp");
+var del = require("del");
+var jscadFiles = require("gulp-jscad-files");
+var merge2 = require("merge2");
+var debug = require("gulp-debug");
+var inject = require("gulp-inject");
+var plumber = require("gulp-plumber");
+var terser = require("gulp-terser");
 
-gulp.task('clean', function(done) {
-  del(['dist/*']).then(paths => {
-    console.log('Deleted files and folders:\n', paths.join('\n')); // eslint-disable-line no-console, no-undef
+gulp.task("clean", function(done) {
+  del(["dist/*"]).then(paths => {
+    console.log("Deleted files and folders:\n", paths.join("\n")); // eslint-disable-line no-console, no-undef
     done();
   });
 });
 
-gulp.task('inject', function() {
+gulp.task("inject", function() {
   return gulp
-    .src('snapfit.jscad')
+    .src("dev.jscad")
     .pipe(plumber())
     .pipe(
       inject(
         merge2(
-          gulp.src(['*.jscad'], { ignore: ['snapfit.jscad'] }),
+          gulp.src(["*.jscad"], { ignore: ["dev.jscad"] }),
           gulp
-            .src('package.json')
+            .src("package.json")
             .pipe(jscadFiles())
             .pipe(
               terser({
@@ -38,44 +38,44 @@ gulp.task('inject', function() {
                 }
               })
             )
-        ).pipe(debug({ title: 'injecting:' })),
+        ).pipe(debug({ title: "injecting:" })),
         {
           relative: true,
-          starttag: '// include:js',
-          endtag: '// endinject',
+          starttag: "// include:js",
+          endtag: "// endinject",
           transform: function(filepath, file) {
-            return '// ' + filepath + '\n' + file.contents.toString('utf8');
+            return "// " + filepath + "\n" + file.contents.toString("utf8");
           }
         }
       )
     )
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest("dist"));
 });
 
 gulp.task(
-  'vuepress',
-  gulp.series(['inject'], function() {
+  "vuepress",
+  gulp.series(["inject"], function() {
     return gulp
-      .src('dist/snapfit.jscad')
-      .pipe(debug({ title: 'vuepress:' }))
-      .pipe(gulp.dest('.vuepress/public/'));
+      .src("dist/snapfit.jscad")
+      .pipe(debug({ title: "vuepress:" }))
+      .pipe(gulp.dest(".vuepress/public/"));
   })
 );
 
 gulp.task(
-  'default',
-  gulp.series(['clean', 'inject'], function() {
+  "default",
+  gulp.series(["clean", "inject"], function() {
     gulp.watch(
-      ['**/*.jscad', 'node_modules/'],
+      ["**/*.jscad", "node_modules/"],
       {
         verbose: true,
         followSymlinks: true,
         delay: 500,
         queue: false,
         ignoreInitial: false,
-        ignored: ['**/*.*~', 'dist/*', '.vuepress/*']
+        ignored: ["**/*.*~", "dist/*", ".vuepress/*"]
       },
-      gulp.series(['inject'])
+      gulp.series(["inject"])
     );
   })
 );
